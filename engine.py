84960ca04440ccf9509ff5566b14fdfd10b9c365
AgnieszkaChS <a.chruszczyk@gmail.com>
2020-01-16 22:03:36 +0100

    Implement initial other character 's movement

--- engine.py
+++ engine.py
@@ -1,3 +1,6 @@
+import random
+
+
 def create_board(width, height):
     '''
     Creates a new game board based on input parameters.
@@ -16,7 +19,7 @@ def create_board(width, height):
 
     for a in range(height - 2):
         board.append(['|'] + (width - 2) * [' '] + ['|'])
-    
+
     board.append([''] + width * ['-'])
 
     return board
@@ -33,17 +36,62 @@ def put_player_on_board(board, player):
     Returns:
     Nothing
     '''
-    x = 0 
+    x = 0
     for row in board:
         y = 0
         for cell in row:
             if cell == player['player_icon']:
                 board[x][y] = ' '
             y += 1
-        x +=1
+        x += 1
 
     height = player['position_y'] - 1
     width = player['position_x'] - 1
     board[height][width] = player['player_icon']
 
     return board
+
+
+def put_other_on_board(board, other):
+    '''
+    Modifies the game board by placing the other character icon at its coordinates.
+
+    Args:
+    list: The game board
+    dictionary: The other character information containing the icon and coordinates
+
+    Returns:
+    Nothing
+    '''
+
+    x = 0
+    for row in board:
+        y = 0
+        for cell in row:
+            if cell == other["other_icon"]:
+                board[x][y] = ' '
+            y += 1
+        x += 1
+
+    # for line in board:
+    #     for cell in line:
+    #         if cell == other["other_icon"]:
+    #             cell = " "
+
+    x_index = other["position_x"]
+    y_index = other["position_y"]
+    board[x_index][y_index] = other["other_icon"]
+
+    return board
+
+
+def get_random_position_of_other(other):
+    random_selection = random.randrange(4)
+    if random_selection == 0:
+        other["position_x"] += other["step"]
+    elif random_selection == 1:
+        other["position_x"] -= other["step"]
+    elif random_selection == 2:
+        other["position_y"] += other["step"]
+    elif random_selection == 3:
+        other["position_y"] -= other["step"]
--- main.py
+++ main.py
@@ -6,8 +6,18 @@ PLAYER_ICON = '@'
 PLAYER_START_X = 6
 PLAYER_START_Y = 10
 
+OTHER_ICON = "M"
+OTHER2_ICON = "K"
+OTHER3_ICON = "C"
+
+OTHER_START_X = 20
+OTHER_START_Y = 30
+
+OTHER_STEP = 1
+
 BOARD_WIDTH = 100
 BOARD_HEIGHT = 30
+
 
 def create_player():
     '''
@@ -21,50 +31,52 @@ def create_player():
 
 def main():
 
-    #player = create_player()
+    # player = create_player()
+    # other = create_other()
+
     player = {'player_icon': PLAYER_ICON, 'position_x': PLAYER_START_X, 'position_y': PLAYER_START_Y}
-
+    other = {'other_icon': OTHER_ICON, 'position_x': OTHER_START_X, 'position_y': OTHER_START_Y, 'step': OTHER_STEP}
     empty_board = engine.create_board(BOARD_WIDTH, BOARD_HEIGHT)
-   
-
 
     is_running = True
 
     while is_running:
 
         board = engine.put_player_on_board(empty_board, player)
+        board = engine.put_other_on_board(board, other)
 
         ui.display_board(board)
 
         key = util.key_pressed()
 
         if key == 'q':
-            is_running = False   
+            is_running = False
 
         elif key == 'w':
             if player['position_y'] == 2:
                 pass
             else:
                 player['position_y'] -= 1
+            engine.get_random_position_of_other(other)
 
         elif key == 's':
             if player['position_y'] == len(board) - 1:
                 pass
             else:
                 player['position_y'] += 1
-
+            engine.get_random_position_of_other(other)
         elif key == 'a':
             if player['position_x'] == 2:
                 pass
             else:
                 player['position_x'] -= 1
-
+            engine.get_random_position_of_other(other)
         elif key == 'd':
             if player['position_x'] == len(board[0]) - 2:
                 pass
             else:
                 player['position_x'] += 1
-
+            engine.get_random_position_of_other(other)
         else:
             pass
         util.clear_screen()
