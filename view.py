506c034e2f962929db2bda1786f53c2e2931553a
Natalia Filipek <nataliafilipek@MacBook-Air-Natalia.local>
2020-01-23 23:03:26 +0100

    improve def print_result

--- view.py
+++ view.py
@@ -1,6 +1,5 @@
 import data_manager
-
-
+from termcolor import colored
 
 BOARD_WIDTH = 100
 BOARD_HEIGHT = 30
@@ -11,27 +10,24 @@ file_results = "results.txt"
 label_results = ["Nickname", "Time", "Points", "Life"]
 
 def start_descriptions():
-    print("Hello and welcome to our cookie game! \n")
-    print("Maybe you want to play some :D? \n")
-    print("So, first you have to give me some informations about You :) \n")
-
+    print(colored("Hello and welcome to our cookie game! \n", "yellow"))
+    print(colored("Maybe you want to play some game:D? \n", "yellow"))
+    print(colored("So, first you have to give me some informations about You or who You want to be in our Magic World :) \n", "yellow"))
 
 def print_result(label, result):
-    print('dupa')
-    print(label)
-    print(result)
-    table_results = {}
-    for elem in result:
-        i = 0
-        for key in label:
-            table_results[key] = elem[i]
-            i += 1    
+    table_results = []
+    table_results.insert(0,label)
+    for line in result:
+        table_results.append(line)
 
-    return table_results
+    i = 1
+    for elem in table_results:
+        print(i, ". ", elem)
+        i += 1 
+    
 
 
 def print_menu(list_options):
-    
     i = 1
     for key in list_options:
         if key != 0:
@@ -45,13 +41,20 @@ def print_table(data):
     print("-" * BOARD_WIDTH)
     for key in data.keys():
         if i == 0:
-            print(key.rjust(int(BOARD_WIDTH/2)),":",data[key])
+            print(colored(key.rjust(int(BOARD_WIDTH/2)),"cyan"),":",colored(data[key], "cyan"))
         else:
             print("|","." * (BOARD_WIDTH - 4),"|")
-            print(key.rjust(int(BOARD_WIDTH/2)),":",data[key])
+            print(colored(key.rjust(int(BOARD_WIDTH/2)), "cyan"),":",colored(data[key], "cyan"))
         i += 1       
     print("-" * BOARD_WIDTH) 
 
+def print_instruction():
+    i = 0
+    while i == 0:
+        print("Hello now I'm empty but in the future....")
+        i += 1
 
 def exit_message():
     print('Goodbay, See you soon!')
+
+
