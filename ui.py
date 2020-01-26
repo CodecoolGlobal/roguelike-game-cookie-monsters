76272e4651e465f1857154644a340208387c9382
Natalia Filipek <nataliafilipek@iPhone-Jakub.local>
2020-01-26 12:50:52 +0100

    nothng changed

--- ui.py
+++ ui.py
@@ -1,11 +1,13 @@
 import main
 
+
 def display_board(board):
     for row in board:
         for cell in row:
-            print(cell, end='')
+            print(colored(cell,"blue"), end='')
         print()
     print()
+
 
 def print_table(inventory):
 
